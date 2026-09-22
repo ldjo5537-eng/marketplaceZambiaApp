4cd46f2a749952741a0ae7d8d04bbd2a8c151acb
ldjo5537-eng <ldjo5537@gmail.com>
2026-09-22 04:46:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ with col1:
             "rencontrées et de ce que vous attendez d'une nouvelle marketplace."
         )
         st.page_link(
-            "pages/1_Questionnaire_Client.py",
+            "1_Questionnaire_Client.py",
             label="Commencer le questionnaire Client",
             icon="➡️",
         )
